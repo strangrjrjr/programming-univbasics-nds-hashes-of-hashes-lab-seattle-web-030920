af9339b91a5c146fd04d557f606e33196bd4e307
Roger Robertson Jr <rjrjr@pm.me>
2020-01-26 19:52:33 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -49,7 +49,7 @@ def naming_system
        label: "Class",
        sub_category: {
          label: "Order",
-         sub_category : {
+         sub_category: {
            label: "Family",
            sub_category: {
              label: "Genus",
